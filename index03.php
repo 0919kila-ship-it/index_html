8c4b3c26fe48f3051abb7aa258de07ed6e21924b
Student2-5\user <0919kila@gmail.com>
2026-03-13 14:29:40 +0800

    3

---
+++ index03.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <title>這是我的第一支程式</title>
+</head>
+<body>
+    <p></p>
+    <?php echo "這是我的第一支程式php<br>\n";//這是未縮寫
+    ?>
+    <?= "這是我的第一支程式php(縮寫)<br>"; //這是縮寫
+     ?> 
+
+    <?= 6+6; 
+     ?> 
+
+
+
+
+</body>
+</html>
